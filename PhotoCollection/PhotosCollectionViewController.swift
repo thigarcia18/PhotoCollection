5876ba5ce5a515d940c8680e6a267fb69ff7cdff
Thiago Garcia <tgalves18@gmail.com>
2018-08-29 23:46:35 -0300

    Create controller for the collection view

---
+++ PhotoCollection/PhotosCollectionViewController.swift
@@ -0,0 +1,60 @@
+//
+//  PhotosCollectionViewController.swift
+//  PhotoCollection
+//
+//  Created by Thiago Garcia on 29/08/18.
+//  Copyright © 2018 Developers Academy. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewController: UICollectionViewController {
+    var photoCategories = PhotoCategory.fetchPhotos()
+    
+    struct Storyboard {
+        static let photoCell = "PhotoCell"
+        static let headerView = "HeaderView"
+        static let showDetailSegue = "ShowDetail"
+        
+        static let leftAndRightPaddings: CGFloat = 2.0
+        static let numberOfItemsPerRow: CGFloat = 3.0
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let collectionViewWidth = collectionView?.frame.width
+        let itemWidth = (collectionViewWidth! - Storyboard.leftAndRightPaddings) / Storyboard.numberOfItemsPerRow
+        
+        let layout = collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: itemWidth, height: itemWidth)
+    }
+    
+    // MARK: - UICollectionViewDataSource
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return photoCategories.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photoCategories[section].imageNames.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Storyboard.photoCell, for: indexPath) as! PhotoCell
+        
+        let photoCategory = photoCategories[indexPath.section]
+        let imageNames = photoCategory.imageNames
+        let imageName = imageNames[indexPath.item]
+        cell.imageName = imageName
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: Storyboard.headerView, for: indexPath) as! HeaderView
+        let category = photoCategories[indexPath.section]
+        headerView.category = category
+        
+        return headerView
+    }
+}
